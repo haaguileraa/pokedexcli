770aafd766abb059e69a57650ca2f5cf5e4bcf1c
hernan <haguil0122@gmail.com>
2026-08-07 22:10:08 +0200

    adding bones logic of an REPL

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		ok := scanner.Scan()
+		if !ok {
+			if err := scanner.Err(); err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println("sacanner could not receive more tokens")
+			break
+		}
+		input := scanner.Text()
+		cleanedInput := cleanInput(input)
+		command := cleanedInput[0]
+		fmt.Println("Your command was:", command)
+	}
+}
